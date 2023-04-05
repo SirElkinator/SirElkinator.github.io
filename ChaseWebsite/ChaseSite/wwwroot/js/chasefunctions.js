87a11164a0901ab091d4618b1226c335043458b8
Chase Sparks <chaspa97@att.net>
2023-04-04 20:45:49 -0600

    remove blazor server
    remove blazor server

--- ChaseWebsite/ChaseSite/wwwroot/js/chasefunctions.js
+++
@@ -1,7 +0,0 @@
-﻿function downloadFromUrl(url, filename) {
-	const anchorElement = document.createElement('a');
-	anchorElement.href = url;
-	anchorElement.download = filename ?? '';
-	anchorElement.click();
-	anchorElement.remove();
-}
