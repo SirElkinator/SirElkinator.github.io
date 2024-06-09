3e93d62e62ca7d3b40a1e8c652852d6220fcf6bd
Chase Sparks <chaspa97.cs@gmail.com>
2024-06-08 21:11:11 -0600

    send new pages to site

---
+++ WyomingElectricalServices/Components/Pages/Home.razor.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace WyomingElectricalServices.Components.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        IJSRuntime JsRuntime { get; set; }
+
+        int currentCount = 0;
+        bool firstRender = true; 
+        //protected async override Task OnAfterRenderAsync()
+        //{
+        //    if (firstRender)
+        //    {
+        //        await JsRuntime.InvokeAsync<object>("initializeCarousel");
+        //        firstRender = false;
+        //    }
+        //}
+    }
+}
