72123b22e4efdb8e37d2f808b2b6ed70a2a02024
SirElkinator <42758063+SirElkinator@users.noreply.github.com>
2024-06-14 01:29:12 +0000

    Deploying to gh-pages from @ SirElkinator/SirElkinator.github.io@210cc5e2ad21ec409918795e4999ba3ef67e3332 🚀

--- js/jscriptfuncs.js
+++ js/jscriptfuncs.js
@@ -1,10 +1,1 @@
-﻿window.initializeCarousel = () => {
-    $('#carouselExampleIndicators').carousel({ interval: 2000 });
-
-    //see step 2 to understand these news id's:
-    $('#carouselExampleIndicators-prev').click(
-        () => $('#carouselExampleIndicators').carousel('prev'));
-    $('#carouselExampleIndicators-next').click(
-        () => $('#carouselExampleIndicators').carousel('next'));
-
-}
+﻿
